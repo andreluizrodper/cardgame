bd1b48d7f83e2c778f73abd2b1366440a4e1be4a
andreluiz <andreluizrodper@gmail.com>
2024-10-09 13:42:23 -0300

    Fixing store issues with nuxt

--- store/index.js
+++ store/index.js
@@ -1,27 +1,26 @@
 import { createStore } from "vuex";
 import { toasts, toastFunctions } from "@/components/ui/toast/store";
 
-export default createStore({
-  state: {
-    user: {},
-    account: null,
-    toasts,
-    redirectMatch: null,
-  },
-  mutations: {
-    ...toastFunctions,
-    setDialog(state, data) {
-      state.dialog = data;
-    },
-    setRedirectMatch(state, data) {
-      state.redirectMatch = data;
-    },
-    setUser(state, data) {
-      state.user = data;
-    },
-    setAccount(state, data) {
-      if (state.account) return;
-      state.account = data;
-    },
-  },
+export const state = () => ({
+  user: {},
+  account: null,
+  toasts,
+  redirectMatch: null,
 });
+
+export const mutations = {
+  ...toastFunctions,
+  setDialog(state, data) {
+    state.dialog = data;
+  },
+  setRedirectMatch(state, data) {
+    state.redirectMatch = data;
+  },
+  setUser(state, data) {
+    state.user = data;
+  },
+  setAccount(state, data) {
+    if (state.account) return;
+    state.account = data;
+  },
+};
