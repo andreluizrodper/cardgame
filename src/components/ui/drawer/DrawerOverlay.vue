c2c0a2d3fdc8db6510a644ef527a42ffb957bc9f
andreluiz <andreluizrodper@gmail.com>
2024-10-10 06:46:22 -0300

    Revert "Update to nuxt"
    This reverts commit ae1d739b823c321a4049399c666b99300bd45c44.

---
+++ src/components/ui/drawer/DrawerOverlay.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { DrawerOverlay } from "vaul-vue";
+import { computed } from "vue";
+import { cn } from "@/lib/utils";
+
+const props = defineProps();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
+
+<template>
+  <DrawerOverlay
+    v-bind="delegatedProps"
+    :class="cn('fixed inset-0 z-50 bg-black/80', props.class)"
+  />
+</template>
